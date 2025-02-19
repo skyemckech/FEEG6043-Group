a3933f7380e8e3ca5ad7fa231d50f0cd7d702043
skyemck <sm4u20@soton.ac.uk>
2025-02-19 16:40:55 +0000

    fuck these files

--- tests/test_laptop.py
+++
@@ -1,7 +0,0 @@
-from Libraries.plot_feeg6043 import plot_zero_order,plot_trajectory,plot_2dframe
-from matplotlib import pyplot as plt
-from Libraries.model_feeg6043 import TrajectoryGenerate
-from Libraries.math_feeg6043 import l2m
-from laptop import LaptopPilot
-
-self.Laptop = LaptopPilot(simulation)
