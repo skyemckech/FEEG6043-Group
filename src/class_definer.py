10122a8a15b9129282ddfd36a631f4727af0a608
skyemck <sm4u20@soton.ac.uk>
2025-02-20 16:49:44 +0000

    testing update

--- src/class_definer.py
+++
@@ -1,9 +0,0 @@
-class OuterClass:
-    def __init__(self):
-        pass
-    class InnerClass:
-        def __init__(self):
-            pass
-        def greet(self):
-            return "Hello from InnerClass!"
-        
