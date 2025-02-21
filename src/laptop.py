1238ad3b20e4feed0fe4a8d0b35a2d9f5b46e115
Connor Cuner <cc4g20@soton.ac.uk>
2025-02-21 19:32:20 +0000

    Fix

--- src/laptop.py
+++ src/laptop.py
@@ -136,29 +136,29 @@ class LaptopPilot:
             "/groundtruth", Pose, self.groundtruth_callback, ip=self.robot_ip
         )
     
-class createExcelFile:
-    def __init__(self, filename = "reference.xlsx"):
-        # Creates workbook and worksheet to modify
-        self.workbook = openpyxl.Workbook()
-        self.worksheet = self.workbook.active
-        self.filename = filename
-        self.workbook.save(filename)
+    class createExcelFile:
+        def __init__(self, filename = "reference.xlsx"):
+            # Creates workbook and worksheet to modify
+            self.workbook = openpyxl.Workbook()
+            self.worksheet = self.workbook.active
+            self.filename = filename
+            self.workbook.save(filename)
 
-        # Initialise variable to store data
-        self.dataLine = []
+            # Initialise variable to store data
+            self.dataLine = []
 
-    def extend_data(self, data):
-        # adds to list self.dataLine
-        data = change_to_list(data)
-        self.dataLine.extend(data)
+        def extend_data(self, data):
+            # adds to list self.dataLine
+            data = change_to_list(data)
+            self.dataLine.extend(data)
 
-    def export_to_excel(self):
-        # appends dataLine to sheet and saves file
-        self.workbook = load_workbook(self.filename)
-        self.worksheet.append(self.dataLine)       
-        self.workbook.save(self.filename)
-        self.workbook.close
-        self.dataLine = []
+        def export_to_excel(self):
+            # appends dataLine to sheet and saves file
+            self.workbook = load_workbook(self.filename)
+            self.worksheet.append(self.dataLine)       
+            self.workbook.save(self.filename)
+            self.workbook.close
+            self.dataLine = []
 
     def true_wheel_speeds_callback(self, msg):
         print("Received sensed wheel speeds: R=", msg.vector.x,", L=", msg.vector.y)
