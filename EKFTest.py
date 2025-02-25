793a8307a777cdab2f33daf4cbd3c2c3be11f20f
Connor Cuner <cc4g20@soton.ac.uk>
2025-02-25 11:04:52 +0000

    update3

--- EKFTest.py
+++ EKFTest.py
@@ -2,35 +2,36 @@
 from Libraries.plot_feeg6043 import plot_kalman
 from Libraries.model_feeg6043 import extended_kalman_filter_predict, extended_kalman_filter_update
 import numpy as np
+import matplotlib.pyplot as plt
 
 class EKF:
     def __init__(self):
         
-            Sigma = np.eye(len(initial_state))
-            mu = np.array(initial_state)
-            zm = Matrix (1,1)
+        Sigma = np.eye(len(initial_state))
+        mu = np.array(initial_state)
+        zm = Matrix (1,1)
 
 
     
-            ##CREATING THE BASE MATRIX FOR Q,U AND R!!!!!########     
-            om = Matrix(1,1); om[0,0] =1
+        ##CREATING THE BASE MATRIX FOR Q,U AND R!!!!!########     
+        om = Matrix(1,1); om[0,0] =1
      
             ### ADDED CONTROL AND PROCESS NOISE!!!!!!!!!########
-            Q = om #measurement noise
-            u = 2*om #control noise
-            R = 2*om #process noise 
-            state = zm; 
-            covariance = om
-            wrapping_index = True
+        Q = om #measurement noise
+        u = 2*om #control noise
+        R = 2*om #process noise 
+        state = zm; 
+        covariance = om
+        wrapping_index = True
 
 
 
 
     def f_nonlintest(x, u, dt):
-            # this is a non-linear model that cannot be solved with a KF f(x)=x**2+u
-            F = np.zeros((1, 1), dtype=float)
-            F[0,0] = 2*x
-            return x ** 2 + u , F
+        # this is a non-linear model that cannot be solved with a KF f(x)=x**2+u
+        F = np.zeros((1, 1), dtype=float)
+        #F[0,0] = 2*x
+        return x ** 2 + u
 
             ##### PLOT TEST #####
 
@@ -39,8 +40,8 @@ class EKF:
     u = 1  # Example input
     dt = 1  # Not used in this function
 
-            # Compute f(x) for each x in x_values
-    f_values = [f_nonlintest(x, u, dt)[0] for x in x_values]
+            # Compute f(x) for each x in x_values  ##[0] for x in x_values
+    f_values = [f_nonlintest(x_values, u, dt)]
 
             # Plot the function
     print(f_values)
