e77daaebcaccc30b849442067a0887cfb0c3406a
Connor Cuner <cc4g20@soton.ac.uk>
2025-02-25 12:59:33 +0000

    update9

--- EKFTest.py
+++ EKFTest.py
@@ -1,70 +1,42 @@
 
-from Libraries.plot_feeg6043 import plot_kalman
-from Libraries.model_feeg6043 import extended_kalman_filter_predict, extended_kalman_filter_update
-from Libraries.math_feeg6043 import Matrix
+from Tutorials.model_feeg6043 import extended_kalman_filter_predict, extended_kalman_filter_update
+from Tutorials.math_feeg6043 import Matrix
 import numpy as np
 import matplotlib.pyplot as plt
 
 class EKF:
     def __init__(self, initial_state):
         
-        #Sigma = np.eye(len(initial_state))
-        #mu = np.array(initial_state)
+        return
 
-        #zm = Matrix(1,1)
-        ##CREATING THE BASE MATRIX FOR Q,U AND R!!!!!########     
-        # om = Matrix(1,1); om[0,0] =1
-     
-            ### ADDED CONTROL AND PROCESS NOISE!!!!!!!!!########
-        # self.Q = om #measurement noise
-        # self.u = 2*om #control noise
-        # self.R = 2*om #process noise 
-        #state = zm 
-        #covariance = om
-        #wrapping_index = True
-
-    def set_noise(self, Q_factor, u_factor, R_factor):
+    def set_parameters(self, Q_factor, u_factor, R_factor):
+        # define a zero matrix (zm) and a one matrix (om)
         om = Matrix(1,1); om[0,0] =1
         zm = Matrix(1,1)
-        self.state = zm 
-        self.Q = Q_factor*om
+
+        # set previous belief and timestep
+        self.state = zm; self.covariance = om
+        self.dt = 1
+
+        # set control and process noise
         self.u = u_factor*om
         self.R = R_factor*om
-        self.covariance = om
 
-        return self.Q, self.u, self.R, self.state, self.covariance
+        # set measurement noise
+        self.z = -2*om
+
+        self.Q = Q_factor*om
+        
+        return self.Q, self.u, self.R, self.z, self.state, self.covariance, self.dt
     
 
     def f_nonlintest(x, u, dt):
             # this is a non-linear model that cannot be solved with a KF f(x)=x**2+u
             F = np.zeros((1, 1), dtype=float)
-            #F[0,0] = 2*x
-            return x ** 2 + u , F
+            F[0,0] = 2*x
+            return x ** 2 + u, F
 
-            ##### PLOT TEST #####
-
-            # Generate x values for plotting
-            
-    x_values = np.linspace(-5, 5, 100)  # Range from -5 to 5
-    u = 1  # Example input
-    dt = 1  # Not used in this function
-
-            # Compute f(x) for each x in x_values
-    f_values = [f_nonlintest(x_values, u, dt)]
-
-            # Plot the function
-    print(f_values)
-    # plt.figure(figsize=(8, 5))
-    # plt.plot(x_values, f_values, label=r'$f(x) = x^2 + u$', color='b')
-    # plt.xlabel('x')
-    # plt.ylabel('f(x)')
-    # plt.title('Non-Linear Function Plot')
-    # plt.axhline(0, color='black', linewidth=0.5, linestyle='--')
-    # plt.axvline(0, color='black', linewidth=0.5, linestyle='--')
-    # plt.legend()
-    # plt.grid()
-    # plt.show()
-
+        
     def h(x):
         H = Matrix(1,1)
         H[0,0] = 1
@@ -73,15 +45,18 @@ class EKF:
     def kalman_filter_process(self,state, covariance, u, f_nonlin, R, dt , z , h , Q , view_flag=True):
 
 
-        pred_state, pred_covariance = extended_kalman_filter_predict(state, covariance, u, f_nonlin, R, dt,view_flag=True)
+        self.pred_state, self.pred_covariance = extended_kalman_filter_predict(state, covariance, u, f_nonlin, R, dt,view_flag=True)
         print('Time predicted is', dt, 's', 'control predicted is', u, 'state predicted is', cor_state, 'covariance predicted is', cor_covariance)
-        cor_state, cor_covariance = extended_kalman_filter_update(pred_state, pred_covariance,z,h,Q,view_flag=True)
-        print('Time is', dt, 's', 'control is', u, 'state is', cor_state, 'covariance is', cor_covariance)
+        # cor_state, cor_covariance = extended_kalman_filter_update(pred_state, pred_covariance,z,h,Q,view_flag=True)
+        # print('Time is', dt, 's', 'control is', u, 'state is', cor_state, 'covariance is', cor_covariance)
 
-        return cor_state, cor_covariance 
+        return self.pred_state, self.pred_covariance 
 
+testexample = EKF
+Q_factor= 2 
+u_factor = 2 
+R_factor = 2
+Q, u, R, z, state, covariance, dt = testexample.set_noise(Q_factor, u_factor, R_factor)
 
-
-Q, u, R, state, covariance = set_noise(self, Q_factor, u_factor, R_factor))
-testexample = class.EKF
-cor_state, cor_covarianc = testexample.kalman_filter_process(testexample.state, covariance, u, f_nonlin, R, dt , z , h , Q , view_flag=True)
+or_state, cor_covarianc = testexample.kalman_filter_process(state, covariance, u, testexample.f_nonlintest, R, dt , z , testexample.h , Q , view_flag=True)
+print(cor_state,cor_covarianc)
