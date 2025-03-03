86efaf4787d63955973bb06c389ee27b09a2fb7d
Connor Cuner <cc4g20@soton.ac.uk>
2025-03-03 19:34:31 +0000

    update 30

--- src/laptop.py
+++ src/laptop.py
@@ -542,12 +542,43 @@ class LaptopPilot:
             self.runtime +=  dt
 
             print("the timestamp is:",self.runtime,"sec")         
+            # ##############Linear##########
+            # if self.runtime <= 7.5:
+
+            #     wheel_speed_msg = Vector3Stamped()
+            #     wheel_speed_msg.vector.x = 1.5*np.pi  #q[0,0] # Right wheelspeed rad/s #### Different Speeds are 0.5/1.0/1.5
+            #     wheel_speed_msg.vector.y = 1.5*np.pi  #q[1,0] # Left wheelspeed rad/s  #### Different Speeds are 0.5/1.0/1.5
+
+            #     self.cmd_wheelrate_right = wheel_speed_msg.vector.x
+            #     self.cmd_wheelrate_left = wheel_speed_msg.vector.y
+            
+            # elif self.runtime <= 25:
+                
+            #     wheel_speed_msg = Vector3Stamped()
+            #     wheel_speed_msg.vector.x = 0*np.pi #q[0,0] # Right wheelspeed rad/s ### 1.0*np.pi
+            #     wheel_speed_msg.vector.y = 0*np.pi  #q[1,0] # Left wheelspeed rad/s #### 1.35*np.pi
+
+            #     self.cmd_wheelrate_right = wheel_speed_msg.vector.x
+            #     self.cmd_wheelrate_left = wheel_speed_msg.vector.y
+
+            # else:
+                
+            #     wheel_speed_msg = Vector3Stamped()
+            #     wheel_speed_msg.vector.x = 0*np.pi #q[0,0] # Right wheelspeed rad/s
+            #     wheel_speed_msg.vector.y = 0*np.pi  #q[1,0] # Left wheelspeed rad/s
+
+            #     self.cmd_wheelrate_right = wheel_speed_msg.vector.x
+            #     self.cmd_wheelrate_left = wheel_speed_msg.vector.y
+
+
+        ########################Rotational##########################
+
 
             if self.runtime <= 7.5:
 
                 wheel_speed_msg = Vector3Stamped()
                 wheel_speed_msg.vector.x = 1*np.pi  #q[0,0] # Right wheelspeed rad/s
-                wheel_speed_msg.vector.y = 1*np.pi  #q[1,0] # Left wheelspeed rad/s
+                wheel_speed_msg.vector.y = 1*np.pi  #q[1,0] # Left wheelspeed rad/s  
 
                 self.cmd_wheelrate_right = wheel_speed_msg.vector.x
                 self.cmd_wheelrate_left = wheel_speed_msg.vector.y
@@ -555,8 +586,8 @@ class LaptopPilot:
             elif self.runtime <= 25:
                 
                 wheel_speed_msg = Vector3Stamped()
-                wheel_speed_msg.vector.x = 1*np.pi #q[0,0] # Right wheelspeed rad/s
-                wheel_speed_msg.vector.y = 1.35*np.pi  #q[1,0] # Left wheelspeed rad/s
+                wheel_speed_msg.vector.x = 1*np.pi #q[0,0] # Right wheelspeed rad/s ### 1.0*np.pi
+                wheel_speed_msg.vector.y = 1.35*np.pi  #q[1,0] # Left wheelspeed rad/s #### 1.35*np.pi
 
                 self.cmd_wheelrate_right = wheel_speed_msg.vector.x
                 self.cmd_wheelrate_left = wheel_speed_msg.vector.y
@@ -569,6 +600,9 @@ class LaptopPilot:
 
                 self.cmd_wheelrate_right = wheel_speed_msg.vector.x
                 self.cmd_wheelrate_left = wheel_speed_msg.vector.y
+
+
+                ##############Linear##########
             
             # if self.runtime <= 0.01:
 
