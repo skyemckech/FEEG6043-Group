46e9fb5a6b0f6ff642154a41d7d1f6c417b044b0
Danae <danaekchaar@hotmail.com>
2025-03-04 11:59:21 +0000

    edited lap

--- src/laptop.py
+++ src/laptop.py
@@ -601,7 +601,7 @@ class LaptopPilot:
                 self.cmd_wheelrate_right = wheel_speed_msg.vector.x
                 self.cmd_wheelrate_left = wheel_speed_msg.vector.y
 
-            elif self.runtime <= 47.5:
+            elif self.runtime <= 60:
                 
                 wheel_speed_msg = Vector3Stamped()
                 wheel_speed_msg.vector.x = 1*np.pi #q[0,0] # Right wheelspeed rad/s ### 1.0*np.pi
