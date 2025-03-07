267cedc864fabf636868a020f4981ca9f038e564
skyemck <sm4u20@soton.ac.uk>
2025-03-07 17:23:24 +0000

    Added plotter for reference position

--- src/show_laptop.py
+++ src/show_laptop.py
@@ -109,10 +109,10 @@ class Window(QWidget):
 
         self.heading_plot.addItem(measured_heading)
         self.heading_plot.addItem(est_heading)
-        self.heading_plot.addItem(p_groundtruth_heading)  
+        #self.heading_plot.addItem(p_groundtruth_heading)  
 
         self.position_plot.addItem(est_position)
-        #self.position_plot.addItem(measured_position)
+        self.position_plot.addItem(measured_position)
         self.position_plot.addItem(waypoints)
         self.position_plot.addItem(lidar)   
         self.position_plot.addItem(p_reference_tracker)
