c75bd938f099144276a033332e3068f7b7b079d9
skyemck <sm4u20@soton.ac.uk>
2025-05-11 17:46:27 +0100

    temp

--- src/test_classifier.py
+++ src/test_classifier.py
@@ -1,4 +1,5 @@
 from Tools import *
+from Libraries import *
 
 cornerClassifier = Classifier()
 cornerClassifier.train_classifier('corner')
@@ -7,3 +8,13 @@ proba = cornerClassifier.classifier.predict_proba([observation.data_filled[:,0]]
 label = (cornerClassifier.classifier.classes_[np.argmax(proba)])
 print(proba)
 print(label)
+
+p = Vector(3); 
+
+p[0] = 0 #Northings
+p[1] = 0 #Eastings
+p[2] = 0 #Heading (rad)
+
+x_bl = 0; y_bl = 0
+lidar = RangeAngleKinematics(x_bl, y_bl, distance_range = [0.1, 1], scan_fov = np.deg2rad(60), n_beams = 120)
+	
